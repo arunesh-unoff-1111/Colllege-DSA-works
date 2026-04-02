6e520b93d55dca57871a68fbe284ae7e1d21d0db
KARUN-0434-25 <aruneshkr9@gmail.com>
2026-04-02 19:56:57 +0530

    updated with work 02-04-2026

--- SEM-II/Class-works/UNIT_02/main_01.cpp
+++ SEM-II/Class-works/UNIT_02/main_01.cpp
@@ -1,5 +1,5 @@
 
-// -------------Insertion-Sort------------//
+//---------------------------------------Insertion-Sort-------------------------------------------------//
 #include <iostream>
 using namespace std;
 
@@ -32,7 +32,7 @@ int main() {
     return 0;
 }
 
-// -------------Bubble-Sort------------//
+// ---------------------------------------Bubble-Sort-------------------------------------------------//
 #include <iostream>
 using namespace std;
 
@@ -65,7 +65,7 @@ int main() {
     return 0;
 }
 
-// -------------Selection-Sort------------//
+// ---------------------------------------Selection-Sort-------------------------------------------------//
 #include<iostream>
 using namespace std;
 
@@ -100,7 +100,7 @@ int main() {
     return 0;
 }
 
-// -------------Counting-Sort------------//
+// ---------------------------------------Counting-Sort-------------------------------------------------//
 #include <iostream>
 using namespace std;
 
@@ -145,39 +145,304 @@ int main() {
     return 0;
 }
 
-// -------------Radix-Sort------------//
-#include<iostream>
+// ---------------------------------------Radix-Sort-------------------------------------------------//
+//---User input without using functions---//
+#include <iostream>
 using namespace std;
-int finding_max(int arr[], int n){
-    int maximum = arr[0];
-    for(int i=0; i<n; i++){
-        if(arr[i] > maximum){
-            maximum = arr[i];
-        }
-    }return maximum;
-}
 
-// void counting_unsorted(int arr[], int n, int exp){
-    
-// }
-int main(){
-
+int main() {
     int n;
-    cout << "Enter the number of elements of array : ";
+    cout << "Enter number of elements: ";
     cin >> n;
 
     int arr[100];
 
-    cout << "Enter the elements of your array : ";
-    for(int i=0; i<n; i++){
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    // radixsorting(arr,n);
-
-    cout << "SORTED ARRAY using radix sorting ==";
-    for(int i=0; i<n; i++){
-        cout << arr[i] << "  ";
+    // Find maximum number
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
     }
+
+    int exp = 1;
+
+    // Radix Sort
+    while(max / exp > 0) {
+        int output[100];
+        int count[10] = {0};
+
+        // Count occurrences of digits
+        for(int i = 0; i < n; i++) {
+            int index = (arr[i] / exp) % 10;
+            count[index]++;
+        }
+
+        // Convert to prefix sum
+        for(int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build output array (IMPORTANT: reverse loop)
+        for(int i = n - 1; i >= 0; i--) {
+            int index = (arr[i] / exp) % 10;
+            output[count[index] - 1] = arr[i];
+            count[index]--;
+        }
+
+        // Copy back to original array
+        for(int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+
+        exp *= 10;
+    }
+
+    // Print sorted array
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+//---User input using functions---//
+#include <iostream>
+using namespace std;
+
+void countingSort(int arr[], int n, int exp) {
+    int output[100];
+    int count[10] = {0};
+
+    for(int i = 0; i < n; i++) {
+        int index = (arr[i] / exp) % 10;
+        count[index]++;
+    }
+
+    for(int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for(int i = n - 1; i >= 0; i--) {
+        int index = (arr[i] / exp) % 10;
+        output[count[index] - 1] = arr[i];
+        count[index]--;
+    }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n) {
+    int max = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    for(int exp = 1; max / exp > 0; exp *= 10) {
+        countingSort(arr, n, exp);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    radixSort(arr, n);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+// ---------------------------------------Bucket-Sort-------------------------------------------------//
+// -----User input | without fucntion-----//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    float arr[100];
+
+    cout << "Enter elements (0 to 1 range):\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Create buckets
+    vector<float> bucket[100];
+
+    // Put elements into buckets
+    for(int i = 0; i < n; i++) {
+        int index = n * arr[i];   // bucket index
+        bucket[index].push_back(arr[i]);
+    }
+
+    // Sort each bucket
+    for(int i = 0; i < n; i++) {
+        sort(bucket[i].begin(), bucket[i].end());
+    }
+
+    // Merge buckets
+    int k = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < bucket[i].size(); j++) {
+            arr[k++] = bucket[i][j];
+        }
+    }
+
+    // Print sorted array
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+// -----User input | without fucntion | withput vectors-----//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter elements (0–99 range):\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Buckets
+    int bucket[10][100];
+    int count[10] = {0};
+
+    // Put elements into buckets
+    for(int i = 0; i < n; i++) {
+        int index = arr[i] / 10;  // bucket index
+        bucket[index][count[index]] = arr[i];
+        count[index]++;
+    }
+
+    // Sort each bucket using Bubble Sort
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < count[i] - 1; j++) {
+            for(int k = 0; k < count[i] - j - 1; k++) {
+                if(bucket[i][k] > bucket[i][k + 1]) {
+                    int temp = bucket[i][k];
+                    bucket[i][k] = bucket[i][k + 1];
+                    bucket[i][k + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Merge buckets back into array
+    int index = 0;
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < count[i]; j++) {
+            arr[index++] = bucket[i][j];
+        }
+    }
+
+    // Print sorted array
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+// ---upper code without arr 100---//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    // Dynamic array
+    int* arr = new int[n];
+
+    cout << "Enter elements (0–99 range):\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Create buckets dynamically
+    int** bucket = new int*[10];
+    int* count = new int[10];
+
+    for(int i = 0; i < 10; i++) {
+        bucket[i] = new int[n];  // each bucket can hold up to n elements
+        count[i] = 0;
+    }
+
+    // Put elements into buckets
+    for(int i = 0; i < n; i++) {
+        int index = arr[i] / 10;
+        bucket[index][count[index]] = arr[i];
+        count[index]++;
+    }
+
+    // Sort each bucket (Bubble Sort)
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < count[i] - 1; j++) {
+            for(int k = 0; k < count[i] - j - 1; k++) {
+                if(bucket[i][k] > bucket[i][k + 1]) {
+                    int temp = bucket[i][k];
+                    bucket[i][k] = bucket[i][k + 1];
+                    bucket[i][k + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Merge buckets
+    int idx = 0;
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < count[i]; j++) {
+            arr[idx++] = bucket[i][j];
+        }
+    }
+
+    // Print result
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Free memory
+    delete[] arr;
+    for(int i = 0; i < 10; i++) {
+        delete[] bucket[i];
+    }
+    delete[] bucket;
+    delete[] count;
+
     return 0;
 }
--- SEM-II/Class-works/UNIT_02/main_02.cpp
+++ SEM-II/Class-works/UNIT_02/main_02.cpp
@@ -1,40 +1,39 @@
 //---codechef wuestion link---https://www.codechef.com/practice/course/strings/STRINGS/problems/WCC----//
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main() {
+int main() {
 
-//     int T;
-//     cin >> T;
+    int T;
+    cin >> T;
 
-//     while (T--) {
-//         int X;
-//         cin >> X;
+    while (T--) {
+        int X;
+        cin >> X;
 
-//         string S;
-//         cin >> S;
+        string S;
+        cin >> S;
 
-//         int carlsen = 0, chef = 0;
+        int carlsen = 0, chef = 0;
 
-//         for (int i = 0; i < 14; i++) {
-//             if (S[i] == 'C') {
-//                 carlsen += 2;
-//             } else if (S[i] == 'N') {
-//                 chef += 2;
-//             } else { // D
-//                 carlsen += 1;
-//                 chef += 1;
-//             }
-//         }
+        for (int i = 0; i < 14; i++) {
+            if (S[i] == 'C') {
+                carlsen += 2;
+            }else if (S[i] == 'N') {
+                chef += 2;
+            }else { // D
+                carlsen += 1;
+                chef += 1;
+            }
+        }
+        if (carlsen > chef) {
+            cout << 60 * X << endl;
+        }else if (carlsen < chef) {
+            cout << 40 * X << endl;
+        }else {
+            cout << 55 * X << endl;
+        }
+    }
 
-//         if (carlsen > chef) {
-//             cout << 60 * X << endl;
-//         } else if (carlsen < chef) {
-//             cout << 40 * X << endl;
-//         } else {
-//             cout << 55 * X << endl;
-//         }
-//     }
-
-//     return 0;
-// }
+    return 0;
+}
