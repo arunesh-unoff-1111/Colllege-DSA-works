d5cc40bd3a943762499297f2d1367f6826d8ef8b
KARUN-0434-25 <aruneshkr9@gmail.com>
2026-04-06 14:39:57 +0530

    updated

--- SEM-II/Class-works/UNIT_01/main_1.cpp
+++ SEM-II/Class-works/UNIT_01/main_1.cpp
@@ -63,3 +63,82 @@ int main() {
     return 0;
 }
 
+//-----------------------------Without-function & with user input
+// -------------Linear-Search------------//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key, found = -1;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = i;
+            break;
+        }
+    }
+
+    if(found != -1)
+        cout << "Element found at index " << found;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter sorted elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1, mid;
+    int found = -1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            found = mid;
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(found != -1)
+        cout << "Element found at index " << found;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
